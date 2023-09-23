5d2abfd957658bb7a6967f5cb6daf21934c6e236
williamparker2023 <you@example.com>
2023-09-23 13:27:57 -0500

    Co-authored-by: bburthardt <bburthardt@users.noreply.github.com>

--- stocks/src/App.tsx
+++ stocks/src/App.tsx
@@ -1,53 +1,8 @@
-// src/App.tsx
-import React, { useState } from 'react';
+import Message from './Message'
 
 function App() {
-  const [symbol, setSymbol] = useState('');
-  const [stockData, setStockData] = useState<any>(null);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(`/api/stocks/${symbol}`);
-      if (response.ok) {
-        const data = await response.json();
-        setStockData(data);
-      } else {
-        setStockData(null);
-      }
-    } catch (error) {
-      console.error('Error fetching stock data:', error);
-      setStockData(null);
-    }
-  };
-
-  return (
-    <div className="App">
-      <h1>Stock Market Website</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="symbol">Stock Symbol:</label>
-        <input
-          type="text"
-          id="symbol"
-          placeholder="Enter stock symbol"
-          value={symbol}
-          onChange={(e) => setSymbol(e.target.value)}
-          required
-        />
-        <button type="submit">Fetch Data</button>
-      </form>
-      <div id="stock-data">
-        {stockData ? (
-          <>
-            <p>Stock Name: {stockData.name}</p>
-            <p>Stock Price: ${stockData.price}</p>
-          </>
-        ) : (
-          <p>Stock not found.</p>
-        )}
-      </div>
-    </div>
-  );
+  return <div><Message></Message></div>
 }
 
 export default App;
+
