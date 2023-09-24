2fd5698c115a61d384c5879bfe16618208eed7bf
williamparker2023 <you@example.com>
2023-09-24 12:38:53 -0500

    asldjkfjasdkjlglads

--- stocks/src/App.jsx
+++ stocks/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import LineGraph from './components/LineGraph';
 import LineGraphAppl from './components/LineGraphAppl';
+import LineGraphMsft from './components/LineGraphMsft';
+import LineGraphGoog from './components/LineGraphGoog';
+import LineGraphAmzn from './components/LineGraphAmzn';
 import Button from "./components/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Nasdaq</h1>
       <LineGraph />
+      <h1>Apple</h1>
       <LineGraphAppl />
+      <h1>Microsoft</h1>
+      <LineGraphMsft />
+      <h1>Google</h1>
+      <LineGraphGoog />  
+      <h1>Amazon</h1>
+      <LineGraphAmzn />
     </div>
   );
 }
---
+++ stocks/src/components/LineGraphAmzn.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { CategoryScale } from 'chart.js';
+import Chart from 'chart.js/auto';
+import Papa from 'papaparse';
+
+const LineGraphAmzn = () => {
+  useEffect(() => {
+    Chart.register(CategoryScale);
+  }, []);
+
+  // State to store the data from the CSV files
+  const [xValues, setXValues] = useState([]);
+  const [yRealData, setYRealData] = useState([]);
+  const [yEstimatedData, setYEstimatedData] = useState([]);
+
+  // Function to fetch and parse CSV data
+  const fetchDataFromCSV = async (csvFilePath) => {
+    try {
+      const response = await fetch(csvFilePath);
+      const csvData = await response.text();
+      const parsedData = Papa.parse(csvData, { header: true });
+
+      // Extract x-values from the first column
+      const xDataValues = parsedData.data.map((row) => parseFloat(row[Object.keys(row)[0]]));
+      // Extract y-values from the second column
+      const yDataValues = parsedData.data.map((row) => parseFloat(row[Object.keys(row)[5]]));
+
+      return { x: xDataValues, y: yDataValues };
+    } catch (error) {
+      console.error('Error fetching or parsing CSV data:', error);
+      return { x: [], y: [] };
+    }
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const realDataValues = await fetchDataFromCSV('old_stonks.csv');
+      const estimatedDataValues = await fetchDataFromCSV('new_stonks.csv');
+
+      // Add 2067 to the x-values from "new_stonks.csv"
+      const adjustedXValues = estimatedDataValues.x.map((x) => x + 2067);
+
+      setXValues([...realDataValues.x, ...adjustedXValues]);
+      setYRealData(realDataValues.y);
+      setYEstimatedData(estimatedDataValues.y);
+    }
+
+    fetchData();
+  }, []);
+
+  const generateEstimatedValues = () => {
+    const xValues = [];
+    for (let i = 0; i < 300; i++) {
+      xValues.push(undefined);
+    }
+    return xValues.concat(yEstimatedData);
+  };
+
+  const yReallyData= yRealData.slice(1767,2068);
+  const xAxis = xValues.slice(1767,2096);
+
+  return (
+    <div style={{ width: '66em', height: '44em' }}>
+      <Line
+        data={{
+          labels: xAxis,
+          datasets: [
+            {
+              label: 'Real Data',
+              data: yReallyData,
+              borderColor: 'red',
+              backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              yAxisID: 'y',
+            },
+            {
+              label: 'Estimated Data',
+              data: generateEstimatedValues(),
+              borderColor: 'green',
+              backgroundColor: 'rgba(0, 255, 0, 0.5)',
+              yAxisID: 'y',
+            },
+          ],
+        }}
+        height={400}
+        width={600}
+        options={{
+          responsive: true,
+          maintainAspectRatio: false,
+        }}
+      />
+    </div>
+  );
+};
+
+export default LineGraphAmzn;
