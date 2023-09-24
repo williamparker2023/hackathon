4572c4f4862743a0204f1c6464d392fd32243bb6
williamparker2023 <you@example.com>
2023-09-23 22:02:32 -0500

    update

--- stocks/src/LineGraph.jsx
+++ stocks/src/LineGraph.jsx
@@ -2,45 +2,42 @@ import React, { useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { CategoryScale } from 'chart.js';
 import Chart from 'chart.js/auto';
+import Papa from 'papaparse';
 
 const LineGraph = () => {
+
+  
+
   useEffect(() => {
     Chart.register(CategoryScale);
   }, []);
 
   return (
-    <div>
+    <div style={{ width: '900px', height: '600px' }}>
       <Line
         data={{
           labels: ['January', 'February', 'March', 'April', 'May'],
           datasets: [
             {
-              label: 'Monthly Sales',
-              data: [65, 59, 80, 81, 56, 77],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-              fill: true, // Set to true if you want to fill the area under the line
+              label: 'Real Data',
+              data: [null, null, null, 412, 435],
+              borderColor: 'red',
+              backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              yAxisID: 'y',
+            },
+            {
+              label: 'Estimated Data',
+              data: [77, 5, 573, 412, null],
+              borderColor: 'green',
+              backgroundColor: 'rgba(0, 255, 0, 0.5)',
+              yAxisID: 'y',
             },
           ],
         }}
         height={400}
         width={600}
         options={{
+          responsive:true,
           maintainAspectRatio: false,
         }}
       />
