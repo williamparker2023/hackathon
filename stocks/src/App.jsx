348ec8327d7852af2d7d073c113848d3346ef6c2
williamparker2023 <you@example.com>
2023-09-24 12:49:38 -0500

    klhbhj

--- stocks/src/App.jsx
+++ stocks/src/App.jsx
@@ -5,12 +5,15 @@ import LineGraphMsft from './components/LineGraphMsft';
 import LineGraphGoog from './components/LineGraphGoog';
 import LineGraphAmzn from './components/LineGraphAmzn';
 import LineGraphNvda from './components/LineGraphNvda';
+import LineGraphTsla from './components/LineGraphTsla';
 import Button from "./components/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1></h1>
+      
       <h1>Nasdaq</h1>
       <LineGraph />
       <h1>Apple</h1>
@@ -23,7 +26,8 @@ function App() {
       <LineGraphAmzn />
       <h1>Nvidia</h1>
       <LineGraphNvda />
-      
+      <h1>Tesla</h1>
+      <LineGraphTsla />
     </div>
   );
 }
